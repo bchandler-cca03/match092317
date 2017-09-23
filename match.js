ebdadeb0a7c9e726015e57a044c536eedb5a8d08
BillChandler <bchandler.cca0@gmail.com>
2017-09-23 06:40:17 -0400

    adding matchNum variable

--- match.js
+++ match.js
@@ -1,10 +1,11 @@
 // Print numbers 1..100 with any number that is 
 // divisible by 3 being replaced by the string MATCH
-var topNum = 10;
+var topNum = 100;
+var matchNum = 5;
 
 for (var i = 0; i <= topNum; i++){
 
-    if( (i%3) == 0){
+    if( (i%matchNum) == 0){
         console.log("MATCH");
     } else {
         console.log("  " + i);
